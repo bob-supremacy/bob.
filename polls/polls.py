7c71fd2f53a828a878069ef8bb133821aec8e1ef
bob-supremacy <66821042+bob-supremacy@users.noreply.github.com>
2021-02-03 21:42:15 +0930

    commit

--- polls/polls.py
+++ polls/polls.py
@@ -3,7 +3,7 @@ import discord
 
 from redbot.core import Config, checks, commands
 
-class poll(commands.Cog):
+class polls(commands.Cog):
     """Commands for Polls"""
     
     def __init__(self, bot):
