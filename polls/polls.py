f5a76e1a28697a6655dd8019c28ec3ae52f61c41
bob-supremacy <66821042+bob-supremacy@users.noreply.github.com>
2021-02-07 23:20:39 +0930

    Update polls.py

--- polls/polls.py
+++ polls/polls.py
@@ -8,3 +8,14 @@ class Polls(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
+        self.conf = Config.get_conf(self, identifier=423187441546)
+        default_guild_settings = {"polls":{},"embed":True}
+        self.conf.register_guild(**default_guild_settings)
+        self.conf.register_global(polls=[])
+        
+    async def red_delete_data_for_user(self, **kwargs):
+        """Nothing to delete."""
+        return
+    
+    
+    
